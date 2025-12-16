3b056cbfba41901a6ea2dc16d766aab8c72290b7
itangaja <afrizaladin33.com>
2025-12-16 15:58:17 +0700

    push

--- backend-servis/routes/web.php
+++ backend-servis/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 // Serve React app for all routes except API
-Route::get('/{any}', function () {
-    return view('app');
-})->where('any', '^(?!api).*$');
+Route::get('/', function () {
+    return response()->json(['status' => 'Backend is running', 'message' => 'Please access the frontend URL']);
+});
 
